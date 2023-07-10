f48983db2474d91421e127f935645a3ca453330a
Abdugaffarov2002 <pulat.96kg@gmail.com>
2023-07-10 18:10:18 +0600

    Initial Commit.

---
+++ main.js
@@ -0,0 +1,149 @@
+// console.log("Hello world");
+
+// let example = "Hello World 2";
+// const example2 = "Hello World 3";
+
+// console.log(example);
+// console.log(example2);
+
+// coment  odno strochniy comment
+
+/* mnogo 
+     strochniy 
+          comment */
+
+// let example3 = 'he said "hello"';
+
+// let name = "Kani";
+// name = "Maker's";
+// console.log(name);
+
+// const name = "Kani";
+// // name = "Makers";
+// console.log(name);
+// --------------------------------------------------------------
+
+// ТИПЫ ДАННЫХ
+
+// 1.STRING
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// let str = "Hello World";
+// let str1 = "Hello ";
+// let str2 = "5";
+// console.log(str, str1, str2);
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// 2. NUMBER
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// let num = 25;
+// let num2 = 12.25;
+// console.log(num, num2);
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// 3. BOOLEAN
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// let bool1 = true; // yes
+// let bool2 = false; // no
+// let bool3 = true;
+// console.log(bool1, bool2, bool3);
+
+// console.log(Boolean(5));
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// 4. UNDEFIND
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// let a;
+// console.log(a);  /// ne spisalnoe pustota
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// 5. NULL
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// let b = null;
+// console.log(b);
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// 6. OBJECT  and ARRAY(MASSIV)
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// let obj = {
+//   name: "Kani",
+//   "last-name": "Arsanbaeva",
+//   age: 22,
+//   hobby: null,
+//   isHuman: true,
+//   dog: {
+//     name: "Orio",
+//     color: "black-white",
+//   },
+// };
+// console.log(obj);
+// console.log(obj.name);
+// console.log(obj.age);
+// console.log(obj["last-name"]);
+// console.log(obj.dog.name);
+// console.log(obj.dog.color);
+
+/// ARRAY - MASSIV
+
+// let arr = [
+//   1,
+//   2,
+//   3,
+//   4,
+//   true,
+//   undefined,
+//   null,
+//   "hello",
+//   { name: "kani" },
+//   [7, 8, 9],
+// ];
+// console.log(arr);
+// console.log(arr[4]);
+// console.log(arr[8]);
+// console.log(arr[6]);
+// console.log(arr[9]);
+// console.log(arr[9][1]);
+// console.log(arr.length);
+// console.log(arr.length - 1);
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// Разбор ///
+
+// task 1
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// let num1 = +prompt("Enter first number");
+// let num2 = +prompt("Enter second number");
+
+// alert(`${num1 + num2}, ${num1 - num2}, ${num1 * num2}, ${num1 / num2}`);
+
+// console.log(num1, num2);
+
+// let add = num1 + num2;
+// let sub = num1 - num2;
+// let mul = num1 * num2;
+// let div = num1 / num2;
+// alert(add);
+// alert(sub);
+// alert(mul);
+// alert(div);
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+/// task 2
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+let a6 = 5 % 3;
+let a7 = 3 % 5;
+let a8 = 5 + "3";
+let a9 = "5" - 3;
+let a10 = 75 + "kg";
+console.log(a6, a7, a8, a9, a10);
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
